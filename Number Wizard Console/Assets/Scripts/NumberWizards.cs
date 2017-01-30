132a4457477846745108a73fbbf2547feead6b7e
Kensaigm <scarroll10@student.cccd.edu>
2017-01-30 00:33:16 -0800

    Put in fix for stuck at 999 error.

--- Number Wizard Console/Assets/Scripts/NumberWizards.cs
+++ Number Wizard Console/Assets/Scripts/NumberWizards.cs
@@ -6,6 +6,7 @@ public class NumberWizards : MonoBehaviour {
 
     int max = 1000;
     int min = 1;
+    int guess = 500;
 
 	// Use this for initialization
 	void Start () {
@@ -15,27 +16,33 @@ public class NumberWizards : MonoBehaviour {
         print("The highest number you can pick is: " + max);
         print("The lowest number you can pick is: " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + "?");
         print("Up Arrow for higher, Down Arrow for lower, and Return for equals.");
 
-
+        max += 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            print("Up arrow pressed.");
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            print("Down arrow pressed.");
-        }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             print("I won!");
+
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // print("Up arrow pressed.");
+            min = guess;
+            guess = (max + min) / 2;
+            print("Higher or lower than " + guess);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // print("Down arrow pressed.");
+            max = guess;
+            guess = (max + min) / 2;
+            print("Higher or lower than " + guess);
         }
-	}
+
+
+    }
 }
